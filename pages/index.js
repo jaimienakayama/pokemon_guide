15b3122b6207862480463b88a85b33fb951a8e4f
jaimienakayama <jaimie.nakayama@icloud.com>
2022-01-28 02:54:26 -0500

    code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-
 import styles from "../styles/Home.module.css";
 
 export default function Home({ pokemon }) {
