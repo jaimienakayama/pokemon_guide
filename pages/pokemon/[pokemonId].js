3aeeb35e049252fd5273f4772a4c6710dd08da91
jaimienakayama <jaimie.nakayama@icloud.com>
2022-01-28 02:17:19 -0500

    added icons and modified markup on columns

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,80 +1,189 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../../styles/Pokemon.module.css";
+import {
+  faBolt,
+  faLeaf,
+  faFireAlt,
+  faTint,
+  faBug,
+  faSkullCrossbones,
+  faFeather,
+  faGlobeAmericas,
+  faMagic,
+  faHandRock,
+  faStarAndCrescent,
+  faGhost,
+  faStarOfLife,
+  faWrench,
+  faSnowflake,
+  faDragon,
+  faMeteor,
+  faAngleDoubleRight,
+  faAngleDoubleLeft,
+  faMoon,
+  faStar,
+  faHandPointRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Pokemon = ({ pokemon }) => {
+  const typeIcon = {
+    normal: faStarOfLife,
+    grass: faLeaf,
+    water: faTint,
+    fire: faFireAlt,
+    bug: faBug,
+    poison: faSkullCrossbones,
+    flying: faFeather,
+    ground: faGlobeAmericas,
+    fairy: faMagic,
+    fighting: faHandRock,
+    psychic: faStarAndCrescent,
+    ghost: faGhost,
+    steel: faWrench,
+    ice: faSnowflake,
+    dragon: faDragon,
+    rock: faMeteor,
+    electric: faBolt,
+    dark: faMoon,
+  };
+
   return (
-    <>
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-        width={500}
-        height={500}
-        alt={pokemon.name}
-      ></Image>
-      <h1>{pokemon.name}</h1>
-      <p>
-        {
-          pokemon.pokemon_v2_pokemonspeciesflavortexts[
-            Math.floor(Math.random() * 5)
-          ].flavor_text
-        }
-      </p>
-      <u>General Info:</u>
-      <ul>
-        <li>
-          <strong>Pokemon No:</strong> {pokemon.id}
-        </li>
-        <li>
-          <strong>Type(s):</strong>
-          <ul>
-            {pokemon.pokemon_v2_pokemons[0].pokemon_v2_pokemontypes.map((p) => {
+    <div>
+      <Link href="/" passHref>
+        <button className={styles.backButton}>BACK</button>
+      </Link>
+      <div className={styles.container}>
+        <div className={styles.upperContent}>
+          <Image
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+            width={500}
+            height={500}
+            alt={pokemon.name}
+          ></Image>
+          <h1>{pokemon.name}</h1>
+          <p>
+            <em>
+              {`"${
+                pokemon.pokemon_v2_pokemonspeciesflavortexts[
+                  Math.floor(Math.random() * 5)
+                ].flavor_text
+              }"`}
+            </em>
+          </p>
+        </div>
+
+        <div className={styles.lowerContent}>
+          <div className={styles.column}>
+            <div className={styles.columnTitle}>SPECIES INFO</div>
+            <div>
+              <strong>Name:</strong> {pokemon.name}
+            </div>
+            <div>
+              <strong>Pokemon No:</strong> {pokemon.id}
+            </div>
+            <strong>Type(s):</strong>
+            <ul>
+              {pokemon.pokemon_v2_pokemons[0].pokemon_v2_pokemontypes.map(
+                (p) => {
+                  return (
+                    <li key={p.pokemon_v2_type.name}>
+                      <FontAwesomeIcon
+                        icon={typeIcon[p.pokemon_v2_type.name]}
+                      />{" "}
+                      {p.pokemon_v2_type.name}{" "}
+                    </li>
+                  );
+                }
+              )}
+            </ul>
+            <div>
+              <strong>Habitat:</strong> {pokemon.pokemon_v2_pokemonhabitat.name}
+            </div>
+            <div>
+              <strong>Base experience:</strong>{" "}
+              {pokemon.pokemon_v2_pokemons[0].base_experience} hp
+            </div>
+            <div>
+              <strong>Weight:</strong>{" "}
+              {Math.ceil((pokemon.pokemon_v2_pokemons[0].weight / 10) * 2.2)}{" "}
+              lbs
+            </div>
+            <div>
+              <strong>Height:</strong>{" "}
+              {((pokemon.pokemon_v2_pokemons[0].height / 10) * 3.281).toFixed(
+                1
+              )}{" "}
+              feet
+            </div>
+          </div>
+
+          <div className={`${styles.column} ${styles.moves}`}>
+            <div className={styles.columnTitle}>POKEMON MOVES</div>
+            {pokemon.pokemon_v2_pokemons[0].pokemon_v2_pokemonmoves.map((p) => {
               return (
-                <li key={p.pokemon_v2_type.name}>{p.pokemon_v2_type.name}</li>
+                <div className={styles.move} key={p.pokemon_v2_move.name}>
+                  <strong>
+                    <em>{p.pokemon_v2_move.name}</em>
+                  </strong>{" "}
+                  <FontAwesomeIcon icon={faHandPointRight} /> power{" "}
+                  <strong>
+                    {p.pokemon_v2_move.power ? p.pokemon_v2_move.power : 0}
+                  </strong>{" "}
+                  - pp <strong>{p.pokemon_v2_move.pp}</strong> - type{" "}
+                  <strong>{p.pokemon_v2_move.pokemon_v2_type.name}</strong>{" "}
+                  <FontAwesomeIcon
+                    icon={typeIcon[p.pokemon_v2_move.pokemon_v2_type.name]}
+                  />
+                </div>
               );
             })}
-          </ul>
-        </li>
-        <li>
-          <strong>Habitat:</strong> {pokemon.pokemon_v2_pokemonhabitat.name}
-        </li>
-        <li>
-          <strong>Base experience:</strong>{" "}
-          {pokemon.pokemon_v2_pokemons[0].base_experience} hp
-        </li>
-        <li>
-          <strong>Weight:</strong>{" "}
-          {Math.ceil((pokemon.pokemon_v2_pokemons[0].weight / 10) * 2.2)} lbs
-        </li>
-        <li>
-          <strong>Height:</strong>{" "}
-          {((pokemon.pokemon_v2_pokemons[0].height / 10) * 3.281).toFixed(1)}{" "}
-          feet
-        </li>
-      </ul>
-      <u>Moves:</u>
-      <ul>
-        {pokemon.pokemon_v2_pokemons[0].pokemon_v2_pokemonmoves.map((p) => {
-          return (
-            <li key={p.pokemon_v2_move.name}>
-              <strong>{p.pokemon_v2_move.name}</strong>: power{" "}
-              {p.pokemon_v2_move.power} - pp {p.pokemon_v2_move.pp} - type{" "}
-              {p.pokemon_v2_move.pokemon_v2_type.name}{" "}
-            </li>
-          );
-        })}
-      </ul>
-      <u>Evolution Chain:</u>
-      <ol>
-        {pokemon.pokemon_v2_evolutionchain.pokemon_v2_pokemonspecies.map(
-          (p) => {
-            return <li key={p.name}>{p.name}</li>;
-          }
-        )}
-      </ol>
-      <Link href="/" passHref>
-        <button>Back</button>
-      </Link>
-    </>
+          </div>
+
+          <div className={`${styles.column} ${styles.moves}`}>
+            <div className={styles.columnTitle}>EVOLUTION</div>
+
+            {pokemon.pokemon_v2_evolutionchain.pokemon_v2_pokemonspecies.map(
+              (p) => {
+                return (
+                  <>
+                    {p.id !== pokemon.id ? (
+                      <Link
+                        key={p.id}
+                        href={`/pokemon/${encodeURIComponent(p.id)}`}
+                        passHref
+                      >
+                        <div key={p.name}>
+                          <span className={styles.evoLink}>
+                            <strong>{p.name}</strong>
+                          </span>{" "}
+                        </div>
+                      </Link>
+                    ) : (
+                      <div className={styles.evoLinkDisabled} key={p.name}>
+                        <FontAwesomeIcon
+                          className={styles.gold}
+                          icon={faStar}
+                        />{" "}
+                        <FontAwesomeIcon icon={faAngleDoubleRight} />{" "}
+                        <strong>{p.name}</strong>{" "}
+                        <FontAwesomeIcon icon={faAngleDoubleLeft} />{" "}
+                        <FontAwesomeIcon
+                          className={styles.gold}
+                          icon={faStar}
+                        />
+                      </div>
+                    )}
+                  </>
+                );
+              }
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -86,59 +195,45 @@ export const getStaticProps = async ({ params }) => {
 
   const { data } = await client.query({
     query: gql`
-      query samplePokeAPIquery {
-        gen3_species: pokemon_v2_pokemonspecies(
-          where: {
-            pokemon_v2_generation: { name: { _eq: "generation-i" } }
-            id: { _eq: ${params.pokemonId} }
-          }
-        ) {
+    query samplePokeAPIquery {
+      gen3_species: pokemon_v2_pokemonspecies(where: {pokemon_v2_generation: {name: {_eq: "generation-i"}}, id: {_eq: ${params.pokemonId}}}) {
+        name
+        id
+        pokemon_v2_pokemonhabitat {
           name
-          id
-          pokemon_v2_pokemonhabitat {
-            name
+        }
+        pokemon_v2_pokemons(limit: 1) {
+          height
+          base_experience
+          weight
+          pokemon_v2_pokemontypes {
+            pokemon_v2_type {
+              name
+            }
           }
-          pokemon_v2_pokemons(limit: 1) {
-            height
-            base_experience
-            weight
-            pokemon_v2_pokemontypes {
+          pokemon_v2_pokemonmoves(distinct_on: move_id, order_by: {move_id: asc}, where: {_and: {pokemon_v2_move: {generation_id: {_eq: 1}}}}) {
+            pokemon_v2_move {
+              name
+              power
+              pp
               pokemon_v2_type {
                 name
               }
             }
-            pokemon_v2_pokemonmoves(
-              distinct_on: move_id
-              order_by: { move_id: asc }
-              where: {
-                _and: { pokemon_v2_move: { generation_id: { _eq: 1 } } }
-              }
-            ) {
-              pokemon_v2_move {
-                name
-                power
-                pp
-                pokemon_v2_type {
-                  name
-                }
-              }
-            }
-          }
-          pokemon_v2_evolutionchain {
-            pokemon_v2_pokemonspecies(where: { generation_id: { _eq: 1 } }) {
-              name
-            }
-          }
-          pokemon_v2_pokemonspeciesflavortexts(
-            where: { pokemon_v2_language: { name: { _eq: "en" } } }
-            order_by: { id: desc }
-            limit: 5
-            offset: 2
-          ) {
-            flavor_text
           }
         }
+        pokemon_v2_evolutionchain {
+          pokemon_v2_pokemonspecies(where: {generation_id: {_eq: 1}}, order_by: {id: asc}) {
+            name
+            id
+          }
+        }
+        pokemon_v2_pokemonspeciesflavortexts(where: {pokemon_v2_language: {name: {_eq: "en"}}}, order_by: {id: desc}, limit: 5, offset: 2) {
+          flavor_text
+        }
       }
+    }
+
     `,
   });
 
